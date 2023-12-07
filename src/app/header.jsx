fe4dfedbe01d1649facd7bdc3f929aa7fd7a05c8
jinwook <77241161+happy-wook-kim@users.noreply.github.com>
2023-12-07 17:55:53 +0900

    [ADD] 파비콘 추가

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -1,18 +1,24 @@
+'use client'
+import { useRouter } from "next/navigation"
 import "./header.css"
 
 export default function Header () {
+  const router = useRouter()
+  const goHome = () => {
+    router.replace('/')
+  }
 
   return (
     <header className="header">
       <section className="header__front">
-        <button className="header__button">
+        <button className="header__button" onClick={goHome}>
           <img className="header--logo" src="../logo.png" alt="로고" />
         </button>
       </section>
       <section className="header__middle">
-        <button className="header__button">메뉴1</button>
+        {/* <button className="header__button">메뉴1</button>
         <button className="header__button">메뉴2</button>
-        <button className="header__button">메뉴3</button>
+        <button className="header__button">메뉴3</button> */}
       </section>
       {/* <section className="header__back">
         바텀
